786afc11c97e448f12d7fdb5e342fd85c59cbd11
Lawrence Forooghian <lawrence@forooghian.com>
2022-02-09 15:25:01 -0300

    Use SSL for Heroku database connection
    (This is required; should have done it in 21f3417.)

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,9 @@ import {TypeOrmModule} from '@nestjs/typeorm';
     TypeOrmModule.forRoot({
       type: "postgres",
       url: process.env.DATABASE_URL || 'postgresql:///test_observation',
+      ssl: process.env.DATABASE_URL ? {
+        rejectUnauthorized: false // Heroku uses self-signed certs
+      } : false, // This is a bit messy; I want SSL for Heroku and not locally 
       migrations: ["dist/migration/*.js"],
       migrationsRun: true
     })
