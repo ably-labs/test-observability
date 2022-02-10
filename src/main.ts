0d4b9c74e13f632fc1197ce736b782de5bc8de92
Lawrence Forooghian <lawrence@forooghian.com>
2022-02-10 14:43:17 -0300

    Require a secret when uploading

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
+  if (!process.env.TEST_OBSERVABILITY_SECRET) {
+    throw new Error("The TEST_OBSERVABILITY_SECRET environment variable must be set.")
+  }
+
   const app = await NestFactory.create(AppModule);
   await app.listen(parseInt(process.env.PORT, 10) || 3000);
 }
--- src/uploads/uploads.controller.ts
+++ src/uploads/uploads.controller.ts
@@ -12,9 +12,13 @@ export class UploadsController {
   }
 
   @Post()
-  async create(@Headers("Content-Type") contentType: string, @Body('junit_report_xml') junitReportXmlBase64: string, @Body('github_sha') githubSha: string, @Body('github_ref_name') githubRefName: string, @Body('github_retention_days') githubRetentionDays: number, @Body('github_action') githubAction: string, @Body('github_run_number') githubRunNumber: number, @Body('github_run_id') githubRunId: string, @Body('iteration') iteration: number): Promise<{id: string}> {
+  async create(@Headers("Test-Observability-Secret") secret: string, @Headers("Content-Type") contentType: string, @Body('junit_report_xml') junitReportXmlBase64: string, @Body('github_sha') githubSha: string, @Body('github_ref_name') githubRefName: string, @Body('github_retention_days') githubRetentionDays: number, @Body('github_action') githubAction: string, @Body('github_run_number') githubRunNumber: number, @Body('github_run_id') githubRunId: string, @Body('iteration') iteration: number): Promise<{id: string}> {
     if (contentType !== "application/json") {
       throw new Error("Expected Content-Type of body to be application/json.")
+    }
+
+    if (secret !== process.env.TEST_OBSERVABILITY_SECRET) {
+      throw new Error("Incorrect value provided in Test-Observability-Secret header.")
     }
 
     const junitReportXml = Buffer.from(junitReportXmlBase64, 'base64').toString('utf8')
