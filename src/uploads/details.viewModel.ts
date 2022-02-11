2dde250e8470cc01acca22191d9fd2d60294975d
Lawrence Forooghian <lawrence@forooghian.com>
2022-02-11 16:46:41 -0300

    Display test failures on upload details page

--- src/uploads/details.viewModel.ts
+++ src/uploads/details.viewModel.ts
@@ -1,7 +1,16 @@
+import {TableViewModel} from "../utils/view/table";
 import {Report} from "./report";
 
 export class ReportDetailsViewModel {
   constructor(private readonly report: Report) {}
 
   readonly title = `Details of upload ${this.report.upload.id}`
+
+  readonly failuresTable: TableViewModel = {
+    headers: ['Test case', 'Message'],
+    rows: this.report.junitReport.failures.map(failure => [
+      {type: "text", text: failure.testCaseName},
+      {type: "text", text: failure.message},
+    ])
+  }
 }
--- src/uploads/junitReport.ts
+++ src/uploads/junitReport.ts
@@ -1,6 +1,11 @@
 import {Upload} from "./upload.entity";
 import {parse, TestSuites} from 'junit2json'
 
+interface Failure {
+  testCaseName: string
+  message: string
+}
+
 export class JUnitReport {
   private constructor(private readonly testSuites: TestSuites) {}
 
@@ -15,5 +20,11 @@ export class JUnitReport {
 
   get numberOfFailures(): number {
     return this.testSuites.failures
+  }
+
+  get failures(): Failure[] {
+    const testCasesHavingFailure = this.testSuites.testsuite.flatMap(testSuite => testSuite.testcase.filter(testCase => testCase.failure))
+
+    return testCasesHavingFailure.flatMap(testCase => testCase.failure.map(details => ({testCaseName: testCase.name, message: details.message})))
   }
 }
