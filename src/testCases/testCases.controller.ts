7314e0aeff92f955cfb6ab5c7b8278f47993dd40
Lawrence Forooghian <lawrence@forooghian.com>
2022-03-24 15:17:41 -0300

    Add API for fetching a test case
    Closes #21.

--- src/testCases/testCases.controller.ts
+++ src/testCases/testCases.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query, Render } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Query,
+  Render,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
 import {
   ReportsService,
   TestCaseUploadsReport,
@@ -6,6 +15,7 @@ import {
 import { ControllerUtils } from 'src/utils/controller/utils';
 import { TestCaseViewModel } from './testCase.viewModel';
 import { TestCasesService } from './testCases.service';
+import { TestCase } from '../uploads/testCase.entity';
 
 @Controller('test_cases')
 export class TestCasesController {
@@ -15,14 +25,15 @@ export class TestCasesController {
   ) {}
 
   @Get(':id')
-  @Render('testCases/details')
   async failureDetails(
     @Param() params: any,
+    @Headers('Accept') accept: string | undefined,
+    @Res() res: Response,
     @Query('branches') branches: string[] | undefined,
     @Query('createdBefore') createdBefore: string | undefined,
     @Query('createdAfter') createdAfter: string | undefined,
     @Query('failureMessage') failureMessage: string | undefined,
-  ): Promise<{ viewModel: TestCaseViewModel }> {
+  ): Promise<void> {
     const filter = ControllerUtils.createFilterFromQuery(
       branches,
       createdBefore,
@@ -31,7 +42,16 @@ export class TestCasesController {
     );
     const testCase = await this.testCasesService.find(params.id, filter);
 
-    return { viewModel: new TestCaseViewModel(testCase, filter) };
+    if (accept === 'application/json') {
+      res.header('Content-Type', 'application/json');
+      const strippedTestCase: Omit<TestCase, 'failures'> &
+        Partial<Pick<TestCase, 'failures'>> = testCase;
+      delete strippedTestCase['failures'];
+      res.json(testCase);
+    } else {
+      const viewModel = new TestCaseViewModel(testCase, filter);
+      res.render('testCases/details', { viewModel });
+    }
   }
 
   @Get(':id/uploads')
